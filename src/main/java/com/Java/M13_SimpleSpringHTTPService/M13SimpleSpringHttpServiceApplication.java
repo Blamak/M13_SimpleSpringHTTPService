116e18e960541d72b578106baadd197dce0c381e
Blamak <vlasherassanchez@gmail.com>
2021-02-07 17:10:00 +0100

    project created

---
+++ src/main/java/com/Java/M13_SimpleSpringHTTPService/M13SimpleSpringHttpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Java.M13_SimpleSpringHTTPService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M13SimpleSpringHttpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M13SimpleSpringHttpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java/M13_SimpleSpringHTTPService/M13SimpleSpringHttpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java.M13_SimpleSpringHTTPService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M13SimpleSpringHttpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
