642ea1d80f548f1081781b12965e5d40b3976873
Blamak <vlasherassanchez@gmail.com>
2021-04-29 15:46:43 +0200

    minor changes

--- src/main/java/com/Java/M13_SimpleSpringHTTPService/Model/DTO/PositionsEnum.java
+++ src/main/java/com/Java/M13_SimpleSpringHTTPService/Model/DTO/PositionsEnum.java
@@ -3,8 +3,10 @@ package com.Java.M13_SimpleSpringHTTPService.Model.DTO;
 /**
  * enum to define every possible job position and its corresponding remuneration
  * 
- * The constants are not stored in database, only showed through the DTO
- */
+ * The salary is not stored in database, only showed through the DTO;
+ * showSalary() method is called in the DTO conversion method of the service layer 
+ * 
+ */ 
 public enum PositionsEnum {
 	
 	CEO(1500), COO(1600), CFO(1700), CIO(1800), CMO(1900), CTO(2000);
